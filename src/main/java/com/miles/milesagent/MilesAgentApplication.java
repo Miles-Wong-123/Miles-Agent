89a7bb6488e05d009d5ee83d8c8b9fb62426b467
Miles <15951521585@163.com>
2026-04-25 00:58:19 +0800

    first commit

---
+++ src/main/java/com/miles/milesagent/MilesAgentApplication.java
@@ -0,0 +1,13 @@
+package com.miles.milesagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilesAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilesAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miles/milesagent/MilesAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miles.milesagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilesAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
